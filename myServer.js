b3caae15f52520f482d1ce71818f1f31c78f5235
pratik <pratikbhai.g@gmail.com>
2019-01-09 10:40:10 +0530

    accept conflict

--- myServer.js
+++ myServer.js
@@ -4,10 +4,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("First change");
   res.write("Second change");
-<<<<<<< HEAD
   res.write("Third change");
-=======
->>>>>>> 21506d20c50e801487e465dc2158bab019fb2bab
   res.write("forth change");
 
   res.end('Hello World!');
